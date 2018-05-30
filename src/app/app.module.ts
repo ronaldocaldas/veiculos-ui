140d5afa2149de0ed5ca0838e4d6c54a740b7d8f
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-30 12:06:53 -0300

    Adicão de navegação com rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
@@ -11,9 +13,17 @@ import { VeiculosModule } from './veiculos/veiculos.module';
 import { ConfirmationService } from 'primeng/api';
 
 import { AppComponent } from './app.component';
+import { VeiculosListagemComponent } from './veiculos/veiculos-listagem/veiculos-listagem.component';
+import { VeiculoCadastroComponent } from './veiculos/veiculo-cadastro/veiculo-cadastro.component';
 
 registerLocaleData(localePt, 'pt-BR');
 
+const routes: Routes = [
+  {
+    path: 'veiculos', component: VeiculosListagemComponent
+  },
+  { path: 'veiculos/novo', component: VeiculoCadastroComponent }
+];
 
 @NgModule({
   declarations: [
@@ -23,6 +33,7 @@ registerLocaleData(localePt, 'pt-BR');
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
+    RouterModule.forRoot(routes),
 
     VeiculosModule,
     CoreModule
