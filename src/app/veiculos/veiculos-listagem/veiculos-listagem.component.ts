cd4aeaeb7ef3f3f2c3cae20b4542f88b2f103218
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-30 00:31:39 -0300

    Criação da tabela com dados e botões de excluir e editar com tooltip e botão de novo lancamento

--- src/app/veiculos/veiculos-listagem/veiculos-listagem.component.ts
+++ src/app/veiculos/veiculos-listagem/veiculos-listagem.component.ts
@@ -5,11 +5,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './veiculos-listagem.component.html',
   styleUrls: ['./veiculos-listagem.component.css']
 })
-export class VeiculosListagemComponent implements OnInit {
+export class VeiculosListagemComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  veiculos = [
+    {
+      marca: 'FIAT',
+      modelo: 'Uno',
+      placa: 'MDK-983',
+       dataLocacao: '01/01/2017',
+      dataDataEntrega: '1/02/2017',
+      locado: true,
+      valor: 10098
+    },
+    {
+      marca: 'CHEVROLET',
+      modelo: 'Corsa',
+      placa: 'IUY-987',
+      valor: 120,
+      dataLocacao: '1/06/2017',
+      dataDataEntrega: '12/08/2017',
+      locado: true
+    },
+    {
+      codigo: 3,
+      marca: 'FORD',
+      modelo: 'Fusion',
+      placa: 'VBN-234',
+      dataLocacao: null,
+      dataDataEntrega: null,
+      locado: false,
+      valor: 220
+    },
+    {
+      codigo: 4,
+      marca: 'VOLKSWAGEN',
+      modelo: 'Golf',
+      placa: 'GAD-931',
+      dataLocacao: null,
+      dataDataEntrega: null,
+      locado: false,
+      valor: 300
+    }
+  ];
 }
--- src/app/veiculos/veiculos.module.ts
+++ src/app/veiculos/veiculos.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VeiculosListagemComponent } from './veiculos-listagem/veiculos-listagem.component';
 
+import { ButtonModule } from 'primeng/components/button/button';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DataTableModule,
+    ButtonModule,
+    CurrencyMaskModule,
+    TooltipModule
+
   ],
   declarations: [VeiculosListagemComponent],
   exports: [VeiculosListagemComponent]
